15c78c97ee3b65a3c38ee32d344d5de4f9ea9317
Phuong <lephuong10142002@gmail.com>
2024-12-26 22:45:26 +0700

    layout and project structure

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "./components/layout/app.header";
+
 function Layout() {
-  return <div>Hello world with Phuongdz</div>;
+  return (
+    <div>
+      <AppHeader />
+      <Outlet />
+    </div>
+  );
 }
 
 export default Layout;
